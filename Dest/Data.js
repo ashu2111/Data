77633b710d134d602d2eb70b92e8737cdf21a452
ashu2111 <105777490+ashu2111@users.noreply.github.com>
2022-05-18 13:30:21 +0530

    Add files via upload

---
+++ Dest/Data.js
@@ -0,0 +1,7 @@
+export const Data = [
+    {
+        image: '../Dest/Sika/jeen_mata_photo.jpg',
+        description: 'Jeenmata is a village in Sikar district of Rajasthan, India. It is located at a distance of 29 km from Sikar town in south. There is an ancient Temple dedicated to Shree Jeen Mataji. Millions of devotees assemble here for a colourful festival held twice in a year in the month of Chaitra and Ashvin during the Navratri',
+        location: ''
+    }
+]
